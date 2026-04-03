b5145433cbbffb0e103eecfb3b8a5c4803c13051
unknown <22499922@stu.mmu.ac.uk>
2026-04-03 16:35:28 +0100

    Added Confidence Scores, Improved API

--- app.py
+++ app.py
@@ -8,21 +8,39 @@ CORS(app)
 # Load model (with vectorizer inside)
 model = joblib.load("sms_fraud_model_MNB_CV.pkl")
 
+@app.route("/")
+def home():
+    return "Spam Detection API is running"
+
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.get_json()
+
+        # Validate input
+        if not data or "text" not in data:
+            return jsonify({"error": "No text provided"}), 400
+
         text = data["text"]
 
-        # Direct prediction (no manual vectorizing)
+        # Prediction
         prediction = model.predict([text])[0]
 
-        result = "Spam" if prediction == 1 else "Not Spam"
+        # Confidence
+        probabilities = model.predict_proba([text])[0]
+        confidence = max(probabilities)
 
-        return jsonify({"prediction": result})
+        # Convert to readable label
+        result = "spam" if prediction == 1 else "Not Spam"
+
+        return jsonify({
+            "prediction": result,
+            "confidence": float(confidence)
+        })
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
