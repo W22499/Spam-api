7e5fd73ab0c231a45ddf4df4d752899f82e8d966
unknown <22499922@stu.mmu.ac.uk>
2026-04-20 20:15:14 +0100

    Added API Security, Input Validation
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# Load API Key
+API_KEY = os.getenv("API_KEY", "v3ryscurkeis") 
+
 # Load model
 data = joblib.load("sms_spam_model_LR_TFIDF_V2.pkl")
-
 model = data["model"]
 threshold = data["threshold"]
 
@@ -18,14 +21,29 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
+        # API Key Authentication
+        client_key = request.headers.get("x-api-key")
+        if client_key != API_KEY:
+            return jsonify({"error": "Unauthorized"}), 401
+
         data = request.get_json()
 
+        # Input Validation
         if not data or "text" not in data:
             return jsonify({"error": "No text provided"}), 400
 
         text = data["text"]
 
-        # Get probability of SPAM (class = 1)
+        if not isinstance(text, str):
+            return jsonify({"error": "Input must be a string"}), 400
+
+        text = text.strip()
+
+        if len(text) == 0:
+            return jsonify({"error": "Empty message"}), 400
+
+
+        # Model prediction
         spam_prob = model.predict_proba([text])[0][1]
         ham_prob = 1 - spam_prob
 
@@ -37,8 +55,8 @@ def predict():
             "ham_probability": float(ham_prob)
         })
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    except Exception:
+        return jsonify({"error": "Internal server error"}), 500
 
 
 if __name__ == "__main__":
