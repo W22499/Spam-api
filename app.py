51942256c3f56bff78c655d95220b31752382dd4
unknown <22499922@stu.mmu.ac.uk>
2026-04-24 16:11:17 +0100

    Final Version

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import joblib
 import os
 
+# Create Flask app instance
 app = Flask(__name__)
 CORS(app)
 
@@ -11,20 +12,23 @@ API_KEY = os.getenv("API_KEY", "v3ryscurkeis")
 
 # Load model
 data = joblib.load("sms_spam_model_LR_TFIDF_V2.pkl")
-model = data["model"]
-threshold = data["threshold"]
+model = data["model"] # Loads Model
+threshold = data["threshold"] # Loads Model Threshold
 
+# Display Message for Render Site
 @app.route("/")
 def home():
     return "Spam Detection API is running"
 
+# API Post Endpoint
 @app.route("/predict", methods=["POST"])
+
 def predict():
     try:
         # API Key Authentication
-        client_key = request.headers.get("x-api-key")
+        client_key = request.headers.get("x-api-key") # Reads API Key from Header
         if client_key != API_KEY:
-            return jsonify({"error": "Unauthorized"}), 401
+            return jsonify({"error": "Unauthorized"}), 401 # Returns 401 if API Key is Invalid
 
         data = request.get_json()
 
@@ -44,20 +48,22 @@ def predict():
 
 
         # Model prediction
-        spam_prob = model.predict_proba([text])[0][1]
-        ham_prob = 1 - spam_prob
+        spam_prob = model.predict_proba([text])[0][1] # Calcs Prob of Spam
+        ham_prob = 1 - spam_prob # Calcs Prob of Real Message
 
-        prediction = "Spam" if spam_prob > threshold else "Not Spam"
+        prediction = "Spam" if spam_prob > threshold else "Not Spam" # If prediction above threshold, then Spam, else Not Spam
 
+        # Returns JSON response with prediction and probabilities
         return jsonify({
             "prediction": prediction,
             "spam_probability": float(spam_prob),
             "ham_probability": float(ham_prob)
         })
 
+    # Crash handling
     except Exception:
         return jsonify({"error": "Internal server error"}), 500
 
-
+# Runs Flask App
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
